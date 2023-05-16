f81a81b6963b7fd3791b411421f44039d6448014
syahlachandrar <syahlachandra@gmail.com>
2023-05-16 20:46:50 +0700

    try make some function

---
+++ JSfunction.js
@@ -0,0 +1,8 @@
+function JenisKendaraan(roda4, roda2) {
+    const objKendaraan = {
+      merknya: roda4,
+      merk: roda2,
+    };
+    console.log(objKendaraan);
+  }
+JenisKendaraan("hrv", "scoopy");
