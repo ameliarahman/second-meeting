f55b0c7397d5dde980a35012532596277cfe6056
clariva27 <meyclariva@gmail.com>
2023-05-22 21:48:29 +0700

    menambahkan file index.js di branch clariva/data

---
+++ index.js
@@ -0,0 +1,11 @@
+function fullName(name, nim) {
+    var objek = {
+      name: name,
+      nim: nim
+    };
+  
+    console.log("Nama saya " + objek.name + "\nNIM saya " + objek.nim);
+  }
+  
+  fullName("Clariva Meydieta Widagdo", "V3922012");
+  
