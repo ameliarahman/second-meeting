4737007c0ec88320cbb5c8bbee95687c34c9c2db
clariva27 <meyclariva@gmail.com>
2023-05-22 21:55:27 +0700

    membuat file berisi array

---
+++ array.js
@@ -0,0 +1,20 @@
+function checkArrayTypes(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] === 'string') {
+        console.log("Ini adalah string");
+      } else if (typeof arr[i] === 'number') {
+        console.log("Ini adalah number");
+      } else if (typeof arr[i] === 'object') {
+        console.log("Ini adalah object");
+      }
+    }
+  }
+  
+  // Contoh pemanggilan function dengan parameter array
+  const array = [1, 2, 3, "Pemrograman Web", {
+    "matkul": "Pemweb",
+    "kampus": "UNS"
+  }];
+  
+  checkArrayTypes(array);
+  
