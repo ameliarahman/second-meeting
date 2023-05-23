66d7febd45b17be4be395f375071bb2f452ebec2
Jenifla <jeniflanurhidayah@gmail.com>
2023-05-24 06:33:00 +0700

    Add new function in jeni.js

--- jeni.js
+++ jeni.js
@@ -7,3 +7,23 @@ function fullName(name, nim) {
   }
   
 fullName("Jeni Fla", "V3922024");
+
+function typeArray(paramA) {
+    for (let i = 0; i < paramA.length; i++) {
+      if (typeof paramA[i] === "string") {
+        console.log("Ini adalah string");
+      } else if (typeof paramA[i] === "number") {
+        console.log("Ini adalah number");
+      } else if (typeof paramA[i] === "object") {
+        console.log("Ini adalah object");
+      }
+    }
+  }
+  
+  const array = [1, 2, 3, "Pemrograman Web", {
+    "matkul": "Pemweb",
+    "kampus": "UNS"
+  }];
+  
+  typeArray(array);
+  
