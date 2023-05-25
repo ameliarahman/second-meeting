892e7c1b40b51f98c998a69b8ce4fe3933cdebc3
Azzahra Kareena <azzahrakareena30@student.uns.ac.id>
2023-05-25 20:10:50 +0700

    Add branch tugas NodeJS

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("Hello world, aku mau belajar programing!!")
+
+var name = "Azzahra Kareena"
+let firstName = "Azzahra"
+const lastName = "Kareena"
+
+console.log(name)
+console.log(firstName)
+console.log(lastName)
+
+if(true){
+    var number1 = 1
+}
+console.log(number1)
+//if(true){ eror karena let tidak bisa di dalam scoope
+//   let number2 = 2
+//}
+//console.log(number2)
+
+const dataNull = null
+console.log(dataNull)
+console.log(typeof dataNull)
+
+let dataUndefined
+console.log(dataUndefined)
+console.log(typeof dataUndefined)
+
+const dataBoolean = true
+console.log(dataBoolean)
+console.log(typeof dataBoolean)
