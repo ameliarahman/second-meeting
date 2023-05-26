741729a6e4e14725b3d5e24828460d4c71d29287
laainra <lailaar77@gmail.com>
2023-05-26 07:46:51 +0700

    add return

--- Laila-NodeJS.js
+++ Laila-NodeJS.js
@@ -4,10 +4,10 @@ function fullName2(nama,nim){
         nim
     }
 
-    console.log(bio);
+    return bio
 }
 
-fullName2("Laila Ainur Rahma", "V3922026");
+console.log(fullName2("Laila Ainur Rahma", "V3922026"));
 
 //  Tugas 2 - function dengan loop
 
@@ -16,16 +16,18 @@ function loop(dataArray){
         const data = dataArray[i];
         
         if (typeof data === "number") {
-          console.log(data + " This is a number");
+          return data + " This is a number";
         } else if (typeof data === "string") {
-          console.log(data + " This is a string");
+          return data + " This is a string";
         } else if (typeof data === "object") {
-          console.log(data + " This is an object");
+          return data + " This is an object";
         }
       }
 }
 
-loop([1, 2, 3, "Pemrograman Web", {
+const newloop = loop([1, 2, 3, "Pemrograman Web", {
   "matkul": "Pemweb",
   "kampus": "UNS"
 }]);
+
+console.log(newloop);
