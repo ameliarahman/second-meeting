f651805de79c1c03241b52748ce40a857ac66d7a
ratripramuditaaa <ratripramudita77@gmail.com>
2023-05-26 13:44:03 +0700

    menambahkan file index.js

---
+++ indeks.js
@@ -0,0 +1,12 @@
+function fullName(name, nim) {
+    var objek = {
+      name: name,
+      nim: nim
+    };
+    
+    console.log(objek);
+  }
+
+  fullName("Ratri Pramudita", "V3922038");
+  
+  git
