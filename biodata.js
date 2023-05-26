c82f26e1090ca1bfdbad8d96f028892432c5fda3
Rafi <rafiii.np@gmail.com>
2023-05-26 21:10:19 +0700

    Add createObject function

---
+++ biodata.js
@@ -0,0 +1,8 @@
+function mahasiswa(nama, nim) {
+  const objmahasiswa = {
+    nama: nama,
+    nim: nim,
+  };
+  console.log(objmahasiswa);
+}
+mahasiswa("Muhammad Rafi Naufal Pratama", "V3922034");
