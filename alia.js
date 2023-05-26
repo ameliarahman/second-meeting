1d5ad696f284d34a895415fb2fab96397b4d07f7
aliadwnt <dewantoalia@gmail.com>
2023-05-26 20:57:16 +0700

    Add File : Alia.js

---
+++ alia.js
@@ -0,0 +1,29 @@
+function fullName(name, nim) {
+    let object  = {
+      Nama: name,
+      Nim : nim
+    };
+    console.log(object);
+  }
+  
+fullName("Alia Dewanto", "V3922004");
+
+function typeArray(paramArray) {
+    for (let i = 0; i < paramArray.length; i++) {
+      if (typeof paramArray[i] === "string") {
+        console.log("Ini adalah string");
+      } else if (typeof paramArray[i] === "number") {
+        console.log("Ini adalah number");
+      } else if (typeof paramArray[i] === "object") {
+        console.log("Ini adalah object");
+      }
+    }
+  }
+  
+  const Array = [1, 2, 3, "Pemrograman Web", {
+    "matkul": "Pemweb",
+    "kampus": "UNS"
+  }];
+  
+  typeArray(Array);
+  
