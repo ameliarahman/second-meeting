8b5a3eb944ee66c4c3f70d48c032bc41bc810eff
Xsanz1 <fabianusjan4@gmail.com>
2023-05-26 21:15:50 +0700

    Add createobject function

---
+++ biodata.js
@@ -0,0 +1,8 @@
+function mahasiswa(nama, nim) {
+    const objmahasiswa = {
+      nama: nama,
+      nim: nim,
+    };
+    console.log(objmahasiswa);
+  }
+  mahasiswa("Fabianus Jan Krisna", "V3922017");
