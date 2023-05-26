4183f311edcb360197c221fa7d3efb78c5b1ec62
iniwahyur <ramadhangaul225@gmail.com>
2023-05-26 18:07:09 +0700

    add createobject function

---
+++ biodata.js
@@ -0,0 +1,8 @@
+function mahasiswa(nama, nim) {
+    const objmahasiswa = {
+      nama: nama,
+      nim: nim,
+    };
+    console.log(objmahasiswa);
+  }
+  mahasiswa("Wahyu Ramadhan", "V3922046");
