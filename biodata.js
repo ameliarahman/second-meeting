14a892c4385d0644f40477c5c0430a93b5f2087f
WidiNug23 <widisuryo11@gmail.com>
2023-05-26 22:29:44 +0700

    Add createObject function

---
+++ biodata.js
@@ -0,0 +1,8 @@
+function mahasiswa(nama, nim) {
+    const objmahasiswa = {
+      nama: nama,
+      nim: nim,
+    };
+    console.log(objmahasiswa);
+  }
+  mahasiswa("Widi Suryo Nugroho", "V3922047");
