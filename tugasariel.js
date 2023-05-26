ab729e8027a24382f08d27c5ce739f42b5970abb
arielfajar <ariel.fajarh@gmail.com>
2023-05-26 20:36:04 +0700

    berhasil menambahkan file tugasariel.js

---
+++ tugasariel.js
@@ -0,0 +1,11 @@
+function fullName(name, nim) {
+    const mahasiswa = {
+      name: name,
+      nim: nim
+    };
+  
+    console.log(mahasiswa);
+  }
+  
+  fullName("Ariel Fajar H", "V3922007");
+  
